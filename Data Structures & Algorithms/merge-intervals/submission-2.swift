7780042e4a832166470cba7860651e28b1e093b7
John Carraher <jkcarraher@gmail.com>
2026-05-09 16:20:11 -0700

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        guard !intervals.isEmpty else { return [] }
+
+        // O(n) - Pre-sort list so we can use an expanding strat
+        let sIntervals: [[Int]] = intervals.sorted{$0[0] < $1[0]}
+
+        // O(n) - Merge Each Interval into the next
+        var retInts: [[Int]] = []
+        var a = sIntervals[0]
+        for b in sIntervals[1...] {
+            if b[0] <= a[1] {
+                // b start is within a
+                if b[1] >= a[1] {
+                    a[1] = b[1]
+                }
+            } else {
+                retInts.append(a)
+                a = b
+            }
+        }
+        retInts.append(a)
+
+        return retInts
+    }
+}
