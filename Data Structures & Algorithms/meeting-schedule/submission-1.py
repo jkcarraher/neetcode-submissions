ca0d2cdc246a442570ddf016662d6d98e442dd43
John Carraher <jkcarraher@gmail.com>
2026-06-30 10:31:35 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) == 0 : return True
+        intervals.sort(key = lambda x: x.start)
+
+        prevEnd = intervals[0].end
+
+        for interval in intervals[1:]:
+            if prevEnd > interval.start :
+                return False
+            prevEnd = interval.end
+        return True
