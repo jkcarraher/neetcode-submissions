a9ebe01ada341a4130a8178b3c6de1a1e99a0897
John Carraher <jkcarraher@gmail.com>
2026-07-01 10:37:04 -0700

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if len(intervals) == 0 : return 0 
+
+        rooms = []
+        intervals.sort(key = lambda x: x.start )
+
+        rooms.append([intervals[0]])
+        for interval in intervals[1:] :
+            for (i, room) in enumerate(rooms) :
+                if interval.start >= room[-1].end:
+                    room.append(interval)
+                    break
+                if i == len(rooms)-1:
+                    rooms.append([interval])
+                    break
+
+        print(len(rooms))
+
+        return len(rooms)
