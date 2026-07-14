1a527ad3e32d7c23daf17bb6ce2fea9fdcf6f433
John Carraher <jkcarraher@gmail.com>
2026-07-14 11:16:28 -0700

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n: number): number {
+        var ret = 0
+        for (let i = 0; i < 32; i++) {
+            const bit = (n >>> i) & 1
+            ret |= bit << (31-i)
+        }
+        return ret >>> 0
+    }
+}
